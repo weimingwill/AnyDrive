59fac8e323a4b35e39e5193716b0476c6d5e87b7
Zhang Ji <zhangji951027@gmail.com>
2014-11-07 04:54:22 +0800

    user now can edit his own booking list

--- php/user_booking.php
+++ php/user_booking.php
@@ -23,18 +23,11 @@ if(!mysqli_select_db($con, 'AnyDrive')) {
     echo "Error creating database: " . mysqli_error();
   }
 }
+$carID_data = $copyNum_data = $userEmail_data = $bookingTime_data = $collectDate_data = $returnDate_data = $cost_data = '';
+$carID_Err = $copyNum_Err = $userEmail_Err = $bookingTime_Err = $collectDate_err = $returnDate_err = $cost_err = '';
 
 
-$cookie_name = "userEmail";
-if(isset($_COOKIE[$cookie_name])){
-  $isLogin = TRUE;
-  $userEmail_data = $_COOKIE[$cookie_name]; 
-} else {
-  $isLogin = False;
-}  
 
-$query = "SELECT * FROM booking where userEmail = '$userEmail_data' ";
-$bookingListResult = mysqli_query($con, $query);
 
 function test_input($data) {
  $data = trim($data);
@@ -51,13 +44,7 @@ $getFieldDelete_str = 'delete';
 
 $isUpdate_str = 'isUpdate';
 $carID_str = "carID";
-$allCarID_query = "SELECT carID FROM car";
-$allCarID_result = mysqli_query($con, $allCarID_query);
 
-$carID_set = array();
-while(($row =  mysqli_fetch_assoc($allCarID_result))) {
-  $carID_set[] = $row[$carID_str];
-}
 
 $copyNum_str = "copyNum";
 $userEmail_str = "userEmail";
@@ -66,32 +53,15 @@ $collectDate_str = "collectDate";
 $returnDate_str = "returnDate";
 $cost_str = "cost";
 
-$allCarID_query = "SELECT carID FROM car";
-$allCarID_result = mysqli_query($con, $allCarID_query);
-$carID_set = array();
-while(($row =  mysqli_fetch_assoc($allCarID_result))) {
-  $carID_set[] = $row[$carID_str];
-}
 
-$allCarCopy_query = "Select carID, copyNum FROM copy";
-$allCarCopy_result = mysqli_query($con, $allCarCopy_query);
-$carCopy_set = array();
-while(($row =  mysqli_fetch_assoc($allCarCopy_result))) {
-  $carCopy_set[] = array($row[$carID_str], $row[$copyNum_str]);
-}
+$cookie_name = "userEmail";
+if(isset($_COOKIE[$cookie_name])){
+  $isLogin = TRUE;
+  $currentUserEmail = $_COOKIE[$cookie_name];
+} else {
+  $isLogin = False;
+}  
 
-$allUserEmail_query = "Select email From User";
-$allUserEmail_result = mysqli_query($con, $allUserEmail_query);
-$userEmail_set = array();
-while(($row =  mysqli_fetch_assoc($allUserEmail_result))) {
-  $userEmail_set[] = $row["email"];
-}
-
-
-
-
-$carID_data = $copyNum_data = $userEmail_data = $bookingTime_data = $collectDate_data = $returnDate_data = $cost_data = '';
-$carID_Err = $copyNum_Err = $userEmail_Err = $bookingTime_Err = $collectDate_err = $returnDate_err = $cost_err = '';
 
 $isInsert = False;
 $isGet = False;
@@ -116,6 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     $row = mysqli_fetch_assoc($bookingResult);
 
+    
     $carID_data = $row[$carID_str];
     $copyNum_data = $row[$copyNum_str];
     $bookingTime_data = $row[$bookingTime_str];
@@ -140,12 +111,10 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  if($_POST[$isUpdate_str] == 'true'){
-    $isOverwrite  = true;
-    $isUpdate = true;
-  }
+  
   $isInsert = TRUE;
-  if (empty($_POST[$carID_str])) {
+ 
+   if (empty($_POST[$carID_str])) {
     $carID_Err = $Err_Required_Field;
     $isInsert = False;
   } else {
@@ -175,7 +144,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $userEmail_data = test_input($_POST[$userEmail_str]);
   } 
 
-
   if (empty($_POST[$collectDate_str])) {
     $collectDate_Err = $Err_Required_Field;
     $isInsert = False;
@@ -191,7 +159,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $returnDate_data = test_input($_POST[$returnDate_str]);
   } 
 
-
   if (empty($_POST[$cost_str])) {
     $cost_Err = $Err_Required_Field;
     $isInsert = False;
@@ -202,26 +169,24 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $cost_data = test_input($_POST[$cost_str]);
   } 
 
-
-
-
-
 }
 
 if($isInsert){
 
-      $updateBooking = "UPDATE booking SET collectDate='$collectDate_data', returnDate='$returnDate_data', cost = '$cost_data'".
-      "WHERE carID = '$carID_data' AND copyNum = '$copyNum_data' AND userEmail = '$userEmail_data' AND bookingTime='$bookingTime_data'";
-      if ( $con->query($updateBooking) === TRUE) {
-        $feedback = "New record updated successfully";
-      } else {  
-        $feedback = "Error: " . $updateBooking . "<br>" . $con->error;
-      }
-    
-    
+  $updateBooking = "UPDATE booking SET collectDate='$collectDate_data', returnDate='$returnDate_data' 
+  WHERE carID = '$carID_data' AND copyNum = '$copyNum_data' AND userEmail = '$userEmail_data' AND bookingTime='$bookingTime_data'";
+  if ( $con->query($updateBooking) === TRUE) {
+    $feedback = "Booking Info updated successfully";
+    // $carID_data = $copyNum_data = $userEmail_data = $bookingTime_data = $collectDate_data = $returnDate_data = $cost_data = '';
+  } else {  
+    $feedback = "Error: " . $updateBooking . "<br>" . $con->error;
+  }
+
+
 }
 // query 
-
+$query = "SELECT * FROM booking where userEmail = '$currentUserEmail' ";
+$bookingListResult = mysqli_query($con, $query);
 
 
 
@@ -237,64 +202,7 @@ if($isInsert){
 
   <div class="container ">
 
-    <div class="page-header" id="banner">
 
-  <div class="tab-pane fade in active" id="Open">
-    <!-- Open Gig -->  
-    <div class="panel  panel-success panel-primary">
-      <!-- Default panel contents -->
-      <div class="panel-heading"><h2>My List of Booking</h2></div>
-      <div class="panel-body">
-      </div>
-      <!-- Table -->
-      <!-- open gigs -->
-      <div class="table-responsive">
-        <table class='table table-hover' border="0">
-          <tr>
-            <th>Car ID</th>
-            <th>Copy Number</th>
-            <th>Booking Time</th>
-            <th>Collect Day</th>
-            <th>Return Day</th>
-            <th>Cost</th>
-            <th>Action</th>
-          </tr>
-
-          
-          <?php
-          if (mysqli_num_rows($bookingListResult) > 0) {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($bookingListResult)) {
-              echo "<tr>";
-              echo "<td>".$row[$carID_str]."</td>";
-              echo "<td>".$row[$copyNum_str]."</td>";
-              echo "<td>".$row[$bookingTime_str]."</td>";
-              echo "<td>".$row[$collectDate_str]."</td>";
-              echo "<td>".$row[$returnDate_str]."</td>";
-              echo "<td>".$row[$cost_str]."</td>";
-
-
-
-              echo "<td><a href='?action=edit&carID=". $row[$carID_str] . "&copyNum=". $row[$copyNum_str].
-              "&userEmail=" . $row[$userEmail_str] . "&bookingTime=" . $row[$bookingTime_str] .
-              "'><button class='btn btn-primary btn-sm col-xs-offset-1 col-sm-4'>edit</button></a>";
-              echo "<a href='?action=delete&carID=". $row[$carID_str] . "&copyNum=". $row[$copyNum_str].
-              "&userEmail=" . $row[$userEmail_str] . "&bookingTime=" . $row[$bookingTime_str] .
-              "'><button class='btn btn-primary btn-sm col-xs-offset-1 col-sm-4'>delete</button></a></td>";
-
-              echo "</tr>";
-
-            }
-          } else {
-            echo "<tr><td colspan='7'><h4> No Booking Available Now</h4></td></tr>";
-          }
-          ?>
-
-        </table>
-      </div>
-    </div>
-  </div>    
-</div>   
 
    <div class="page-header" id="banner">
     <h1>Booking Info<small>&nbsp<?php echo $feedback;?></small></h1>
@@ -303,139 +211,143 @@ if($isInsert){
       <div class="form-group">
         <label for="carID" class="col-lg-2 control-label">Car ID*</label>
         <div class="col-lg-6 ">
-          <input readonly type="text" name="carID" class="form-control" id="carID"  >
+          <input readonly type="text" name="carID" class="form-control" id="carID" value="<?php echo $carID_data; ?>" >
         </div>
-      <div class = "col-lg-4">
-        <span class="text-danger"><?php echo $carID_Err;?></span>
+        <div class = "col-lg-4">
+          <span class="text-danger"><?php echo $carID_Err;?></span>
+        </div>
       </div>
-    </div>
 
-    <div class="form-group">
-      <label for="copyNum" class="col-lg-2 control-label">Copy Number*</label>
-      <div class="col-lg-6 id=""">
-        <select id="copyNum"
-        <?php 
-        if($isUpdate){
-          echo "disabled";
-        }
-        ?>
-        type="text" name="copyNum" class="form-control"  >
-        <?php 
-        if(!empty($copyNum_data)){
-          echo "<option value='$copyNum_data' selected='selected'>". $copyNum_data ."</option>";
-        }
-        ?>
-      </select>
-      <?php 
-      if($isUpdate){
-        echo "<input type='hidden' name='copyNum' value=". $copyNum_data  . ">";
-      }
-      ?>
+      <div class="form-group">
+        <label for="copyNum" class="col-lg-2 control-label">Copy Num*</label>
+        <div class="col-lg-6 ">
+          <input readonly type="text" name="copyNum" class="form-control" id="copyNum" value="<?php echo $copyNum_data; ?>" >
+        </div>
+        <div class = "col-lg-4">
+          <span class="text-danger"><?php echo $copyNum_Err;?></span>
+        </div>
+      </div>
 
-    </div>
-    <div class = "col-lg-4">
-      <span class="text-danger"><?php echo $copyNum_Err;?></span>
-    </div>
+
+        
+        
+      <input  type="hidden" name="userEmail" class="form-control" id="userEmail" value="<?php echo $userEmail_data; ?>" >
+        
+
+
+      <div class="form-group">
+        <label for="bookingTime" class="col-lg-2 control-label">Booking Time*</label>
+        <div class="col-lg-6 ">
+          <input readonly type="text" name="bookingTime" class="form-control" id="bookingTime" value="<?php echo $bookingTime_data; ?>" >
+        </div>
+        <div class = "col-lg-4">
+          <span class="text-danger"><?php echo $bookingTime_Err;?></span>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="cost" class="col-lg-2 control-label">Cost($)</label>
+        <div class="col-lg-6"> 
+          <input readonly type="number" name="cost" class="form-control" id="cost" value="<?php echo $cost_data;?>" >
+
+        </div>
+        <div class = "col-lg-4">
+          <span class="text-danger"><?php echo $cost_Err;?></span>
+        </div>
+      </div>
+
+
+      <div class="form-group">
+        <label for="collectDate" class="col-lg-2 control-label">Collect Date</label>
+        <div class=" col-lg-6">
+          <input name="collectDate"  value="<?php echo $collectDate_data;?>" placeholder="Select Date" class="form-control" data-date-format="YYYY-MM-DD" id='collectDate'>
+        </div>
+        <div class = "col-lg-4">
+          <span class="text-danger"><?php echo $collectDate_Err;?></span>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="returnDate" class="col-lg-2 control-label">Return Date</label>
+        <div class=" col-lg-6">
+          <input name="returnDate"  value="<?php echo $returnDate_data;?>" placeholder="Select Date" class="form-control" data-date-format="YYYY-MM-DD" id='returnDate'>
+        </div>
+        <div class = "col-lg-4">
+          <span class="text-danger"><?php echo $returnDate_Err;?></span>
+        </div>
+      </div>
+
+      
+
+
+
+      <div class="form-group">
+        <div class="col-lg-10 col-lg-offset-2">
+          <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+      </div>
+
+    </form>
   </div>
 
+  <div class="page-header" id="banner">
+
+    <div class="tab-pane fade in active" id="Open">
+      <!-- Open Gig -->  
+      <div class="panel  panel-default panel-primary">
+        <!-- Default panel contents -->
+        <div class="panel-heading"><h2>My List of Booking</h2></div>
+        <div class="panel-body">
+        </div>
+        <!-- Table -->
+        <!-- open gigs -->
+        <div class="table-responsive">
+          <table class='table table-hover' border="0">
+            <tr>
+              <th>Car ID</th>
+              <th>Copy Number</th>
+              <th>Booking Time</th>
+              <th>Collect Day</th>
+              <th>Return Day</th>
+              <th>Cost</th>
+              <th>Action</th>
+            </tr>
+
+
+            <?php
+            if (mysqli_num_rows($bookingListResult) > 0) {
+            // output data of each row
+              while($row = mysqli_fetch_assoc($bookingListResult)) {
+                echo "<tr>";
+                echo "<td>".$row[$carID_str]."</td>";
+                echo "<td>".$row[$copyNum_str]."</td>";
+                echo "<td>".$row[$bookingTime_str]."</td>";
+                echo "<td>".$row[$collectDate_str]."</td>";
+                echo "<td>".$row[$returnDate_str]."</td>";
+                echo "<td>".$row[$cost_str]."</td>";
 
 
 
+                echo "<td><a href='?action=edit&carID=". $row[$carID_str] . "&copyNum=". $row[$copyNum_str].
+                "&userEmail=" . $row[$userEmail_str] . "&bookingTime=" . $row[$bookingTime_str] .
+                "'><button class='btn btn-primary btn-sm col-xs-offset-1 col-sm-4'>edit</button></a>";
+                echo "<a href='?action=delete&carID=". $row[$carID_str] . "&copyNum=". $row[$copyNum_str].
+                "&userEmail=" . $row[$userEmail_str] . "&bookingTime=" . $row[$bookingTime_str] .
+                "'><button class='btn btn-primary btn-sm col-xs-offset-1 col-sm-4'>delete</button></a></td>";
 
+                echo "</tr>";
 
+              }
+            } else {
+              echo "<tr><td colspan='7'><h4> No Booking Available Now</h4></td></tr>";
+            }
+            ?>
 
-  <div class="form-group">
-    <label for="userEmail" class="col-lg-2 control-label">User email*</label>
-    <div class="col-lg-6 ">
-      <select 
-      <?php 
-      if($isUpdate){
-        echo "disabled";
-      }
-      ?>
-      type="text" name="userEmail" class="form-control" id="userEmail" >
-      <?php
-      for($x=0;$x<count($userEmail_set);$x++) {
-
-        echo "<option value='$userEmail_set[$x]' ";
-        if($userEmail_set[$x] == $userEmail_data) {
-          echo "selected='selected'";
-        }
-        echo ">" . $userEmail_set[$x] . "</option>"; 
-      }
-      ?>
-    </select>
-    <?php 
-    if($isUpdate){
-      echo "<input type='hidden' name='userEmail' value=". $userEmail_data  . ">";
-    }
-    ?>
-
-  </div>
-  <div class = "col-lg-4">
-    <span class="text-danger"><?php echo $carID_Err;?></span>
-  </div>
-</div>
-
-<div class="form-group">
-  <label for="bookingTime" class="col-lg-2 control-label">Booking Time</label>
-  <div class=" col-lg-6">
-    <input name="bookingTime"
-    <?php 
-    if($isUpdate){
-      echo "readonly";
-    }
-    ?>
-    value="<?php echo $bookingTime_data;?>" placeholder="Select Time" class="form-control"  id='bookingTime'>
-  </div>
-  <div class = "col-lg-4">
-    <span class="text-danger"><?php echo $bookingTime_Err;?></span>
-  </div>
-</div>
-
-<div class="form-group">
-  <label for="collectDate" class="col-lg-2 control-label">Collect Date</label>
-  <div class=" col-lg-6">
-    <input name="collectDate"  value="<?php echo $collectDate_data;?>" placeholder="Select Date" class="form-control" data-date-format="YYYY-MM-DD" id='collectDate'>
-  </div>
-  <div class = "col-lg-4">
-    <span class="text-danger"><?php echo $collectDate_Err;?></span>
-  </div>
-</div>
-
-<div class="form-group">
-  <label for="returnDate" class="col-lg-2 control-label">Return Date</label>
-  <div class=" col-lg-6">
-    <input name="returnDate"  value="<?php echo $returnDate_data;?>" placeholder="Select Date" class="form-control" data-date-format="YYYY-MM-DD" id='returnDate'>
-  </div>
-  <div class = "col-lg-4">
-    <span class="text-danger"><?php echo $returnDate_Err;?></span>
-  </div>
-</div>
-
-<div class="form-group">
-  <label for="cost" class="col-lg-2 control-label">Cost*</label>
-  <div class="col-lg-6"> 
-    <input type="number" name="cost" class="form-control" id="cost" value="<?php echo $cost_data;?>" >
-
-  </div>
-  <div class = "col-lg-4">
-    <span class="text-danger"><?php echo $cost_Err;?></span>
-  </div>
-</div>
-
-   
-
-<div class="form-group">
-  <div class="col-lg-10 col-lg-offset-2">
-    <button type="submit" class="btn btn-primary">Update</button>
-  </div>
-</div>
-
-</form>
-</div>
-
-
+          </table>
+        </div>
+      </div>
+    </div>    
+  </div> 
 
 
 </div><!--body part-->
@@ -448,16 +360,7 @@ if($isInsert){
 
 $(function(){
 
-  $('#bookingTime').datetimepicker({
-    format: 'YY-MM-DD hh:mm:ss',
-    pickDate: true,
-    pickTime: true,
-    useMinutes: true,
-    useSeconds: true,
-    useCurrent: true,
-    pick12HourFormat: false,
-  });
-  $('#bookingTime').datetimepicker();
+  
 
   $('#collectDate').datetimepicker({
     pickTime: false
